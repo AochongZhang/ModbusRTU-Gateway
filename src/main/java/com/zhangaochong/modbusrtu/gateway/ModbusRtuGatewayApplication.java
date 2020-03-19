bc9cf794555464658d1dc13dc9f1a8af87e29c18
AochongZhang <aochongzhang@gmail.com>
2020-03-19 19:21:36 +0800

    init

---
+++ src/main/java/com/zhangaochong/modbusrtu/gateway/ModbusRtuGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zhangaochong.modbusrtu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusRtuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModbusRtuGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangaochong/modbusrtu/gateway/ModbusRtuGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangaochong.modbusrtu.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModbusRtuGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
